05607ada20c9c754b748a489c57bd86d91dbc9a4
Francisco Batista <fbatista.fxbi@gmail.com>
2018-12-01 15:28:05 -0600

    Ahora trae el id del equipo en el login

--- application/resources/Sesiones/Sesiones_Model.js
+++ application/resources/Sesiones/Sesiones_Model.js
@@ -24,6 +24,16 @@ class Sesiones_Model {
             })
         })
     }
+
+    getAuthData(correo) {
+        const queryString = "SELECT id, contrasena, sal FROM usuarios WHERE correo = ?";
+        return pool.query(queryString, [correo]);
+    }
+
+    checkSession(token) {
+        const queryString = "SELECT u.*, ANY_VALUE(ue.idEquipo) AS equipo FROM sesiones AS s INNER JOIN usuarios AS u ON s.usuarioId = u.id LEFT JOIN usuarios_equipos ue on u.id = ue.idUsuario GROUP BY id";
+        return pool.query(queryString, [token]);
+    }
 }
 
 export default Sesiones_Model;
